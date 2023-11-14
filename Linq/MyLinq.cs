42898ccc6307dbca3d1bf276d7a2c70bdd167fbb
Евгений Ремхе <Evgeny.Remhe@testdlab.com>
2023-11-14 22:09:06 +0700

    Linq: - Select - Where - Any - Contains - ToList

---
+++ Linq/MyLinq.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Linq
+{
+    public static class MyLinq
+    {
+        public static IEnumerable<TResult> Select<TSource, TResult>(
+            this IEnumerable<TSource> source,
+            Func<TSource, TResult> selector)
+        {
+            if (source == null || selector == null)
+            {
+                return null;
+            }
+            
+            List<TResult> result = new List<TResult>();
+
+            foreach (var item in source)
+            {
+                result.Add(selector.Invoke(item));
+            }
+
+            return result;
+        }
+
+        public static IEnumerable<TSource> Where<TSource>(
+            this IEnumerable<TSource> source,
+            Predicate<TSource> predicate)
+        {
+            if (source == null || predicate == null)
+            {
+                return null;
+            }
+
+            List<TSource> result = new List<TSource>();
+
+            foreach (var item in source)
+            {
+                if (predicate.Invoke(item))
+                {
+                    result.Add(item);
+                }
+            }
+            
+            result.Sum()
+
+            return result;
+        }
+
+        public static bool Any<TSource>(this IEnumerable<TSource> source, Predicate<TSource> predicate)
+        {
+            if (source == null || predicate == null)
+            {
+                return false;
+            }
+
+            foreach (var item in source)
+            {
+                if (predicate(item))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public static bool Contains<TSource>(this IEnumerable<TSource> source, TSource item)
+        {
+            if (source == null || item == null)
+            {
+                return false;
+            }
+
+            foreach (var sourceItem in source)
+            {
+                if (sourceItem.GetHashCode().Equals(item.GetHashCode()))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+        
+        
+
+        public static List<T> ToList<T>(this IEnumerable<T> source)
+        {
+            return new List<T>(source);
+        }
+    }
+}
--- Linq/Program.cs
+++ Linq/Program.cs
@@ -1,9 +1,28 @@
-﻿namespace Linq
+﻿using System;
+
+namespace Linq
 {
     internal class Program
     {
         public static void Main(string[] args)
         {
+            var list = new[]
+            {
+                1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+            };
+
+            var result = list.Select(l => l * l)
+                .Where(l => l % 2 == 0);
+
+            foreach (var item in result)
+            {
+                Console.Write($"{item} ");
+            }
+
+            Console.WriteLine("");
+            
+            Console.WriteLine(result.Any(i => i > 50));
+            Console.WriteLine(result.Contains(4));
         }
     }
 }
